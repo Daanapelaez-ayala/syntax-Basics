f8f8b47efe123aa79bbffac8fe653d595df30f4f
Daana <126dpelaezayala@frhsd.com>
2024-10-10 12:48:01 -0400

    Arrays

---
+++ js/sandbox.js
@@ -0,0 +1,22 @@
+let ninjas = ['shuan', 'ryu','chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let age =[20, 25, 30,35];
+// console.log(ages[2]);
+
+// let random = ['shuan', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninja.length);
+
+// array methods
+
+// let result = ninjas.join('-')
+// let result = ninja.indexOf('chun-li');
+// let result =ninjas.concat(['ken','crystals']);
+let result =ninjas.push('ken');
+ result = ninjas.pop();
+
+console.log(result);
